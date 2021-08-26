659d6ba12b785928d94a24cdf13cd5f8ac3d5cd7
Partha Ghosh <38184323+codeunik@users.noreply.github.com>
2021-08-26 11:58:31 +0000

    Update chroot.py

--- chroot.py
+++ chroot.py
@@ -22,6 +22,18 @@ with open('/etc/hosts', 'a') as f:
 ::1		localhost
 127.0.1.1	""" + hostname + ".localdomain	" + hostname + '\n')
 
+with open("/etc/modprobe.d/g14-prohibited-drivers.conf", "w") as f:
+    f.write('''blacklist nouveau
+blacklist nvidiafb
+blacklist rivafb
+blacklist i2c_nvidia_gpu
+''')
+
+with open("/etc/modprobe.d/g14-nvidia.conf", "w") as f:
+    f.write('''options nvidia_drm modeset=1
+options nvidia "NVreg_DynamicPowerManagement=0x02'
+''')
+    
 # Initramfs
 exec_cmd("mkinitcpio -P")
 
@@ -33,33 +45,21 @@ exec_cmd(
 exec_cmd("passwd " + username)
 exec_cmd("EDITOR=nano visudo")
 
-exec_cmd(
-    "reflector --latest 200 --country Sweden --country Japan --country India --country \"United States\" --country France --country Germany  --age 48 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
-)
 exec_cmd("pacman -Syyu " + " ".join(packages))
 
 exec_cmd("systemctl enable " + " ".join(services))
 
 if bootloader == 'grub':
     exec_cmd(
-        "grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi"
+        "grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot"
     )
-    exec_cmd("grub-mkconfig -o /boot/grub/grub.cfg")
-if bootloader == 'systemd-boot':
-    exec_cmd("bootctl --path=/boot install")
-    with open("/boot/loader/loader.conf", 'w') as f:
-        f.write("default arch\ntimeout 4\nconsole-mode max\neditor no\n")
-    root_blkid = os.popen("blkid " + root).read()
-    root_uuid = re.search(r".*UUID=\"(.*?)\"\ .*", root_blkid).group(1)
-    with open("/boot/loader/entries/arch.conf", 'w') as f:
-        f.write("title   Arch Linux\n" + "linux   /vmlinuz-linux\n" +
-                "initrd  /" + cpu + "-ucode.img\n" +
-                "initrd  /initramfs-linux.img\n" + "options root=UUID=" +
-                root_uuid + " rw\n")
+    exec_cmd("grub-mkconfig -o /boot/grub/grub.cfg") 
 
-if will_reinstall:
-    exec_cmd(
-        "mkdir -p /home/partha/pkg/ && cp -r /var/cache/pacman/pkg/* /home/partha/pkg/"
-    )
+exec_cmd("su - " + username)
+exec_cmd('export MAKEFLAGS="-j16"')
+exec_cmd("mkdir aur && cd aur")
+exec_cmd("git clone https://aur.archlinux.org/rog-core.git && cd rog-core && makepkg -si && cd ..")
+exec_cmd("sudo systemctl enable rog-core.service")
+exec_cmd("git clone https://aur.archlinux.org/asusctl-git.git && cd  asusctl-git && makepkg -si && cd ..")
 
 exec_cmd("exit")
