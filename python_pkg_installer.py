4d835315239d26a6e4674c3e69e71aa4dd5a8bc3
Partha Ghosh <35074043+partha-ghosh@users.noreply.github.com>
2022-08-20 22:49:25 +0200

    Create python_pkg_installer.py

---
+++ python_pkg_installer.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("conda install pytorch torchvision torchaudio cudatoolkit=11.3 -c pytorch")
+os.system("conda install -c anaconda pandas numpy scipy")
+os.system("conda install -c conda-forge matplotlib")
